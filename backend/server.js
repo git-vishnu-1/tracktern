5c1ee6ad2fc12374f51bc83ec7696d5458f4540d
git-vishnu-1 <mastershifu0990@gmail.com>
2025-12-29 22:03:09 +0530

    Add Express server with test API route

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// test route
+// root test route
 app.get("/", (req, res) => {
   res.send("Internship Application Management System Backend Running");
+});
+
+// API test route
+app.get("/api/test", (req, res) => {
+  res.json({ message: "API working" });
 });
 
 const PORT = 5000;
